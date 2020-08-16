fc40259721de6e347d66b30246cf9cee5e751b9c
N0Ngen <n0ngen@tutanota.com>
2020-08-16 21:40:23 +0200

    add an admin account, link models with the admin page

--- cSite/admin.py
+++ cSite/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.db import models
+from .models import Tutorial
 
 # Register your models here.
+
+class cardview(admin.ModelAdmin):
+  fieldsets = [
+    ('Title/Date', {'fields': ['tut_title', 'tut_PD']}),
+    ('Content', {'fields': ['tut_content']}),
+  ]
+
+admin.site.register(Tutorial, cardview)
--- cSite/models.py
+++ cSite/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class Tutorial(models.Model):
   tut_title = models.CharField(max_length=200)
   tut_content = models.TextField()
-  tut_PD = models.DateTimeField()
+  tut_PD = models.DateTimeField('Date published', default=datetime.now())
 
   def __str__(self):
     return self.tut_title
